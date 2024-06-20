2b3f0ff24b191198479db0f02b49759212d1c94e
elammz <elammizanu4@gmail.com>
2024-06-20 20:20:58 +0300

    new file:   python_introduction/basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+sum = int(number1) + int(number2) 
+difference = int(number1) -int(number2)
+product = int(number1) * int(number2)
+print("Addition of ", number1, "and ", number2, "is ", sum)
+print("Subtraction of ", number1 , "and ", number2 , "is " , difference)
+print("Multiplication of ", number1, "and ", number2, "is ", product)
+
+
