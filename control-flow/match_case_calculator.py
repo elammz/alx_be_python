8cdfcd8c65fa1c6b31298892755e999e764fd970
elammz <elammizanu4@gmail.com>
2024-06-30 12:08:10 +0300

    added result again

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,13 +4,13 @@ operations = input("Choose the operation (+, -, *, /): ")
 match operations:
     case "+":
         result = num1 + num2
-        print("The result is", result, ".")
+        print(f"The result is {result}.")
     case "-":
         result = num1 - num2
-        print("The result is", result, ".")
+        print(f"The result is {result}.")
     case "*":
         result = num1 * num2
-        print("The result is", result, ".")
+        print(f"The result is {result}.")
     case "/":
         if num2 != 0:
             result = num1 / num2
