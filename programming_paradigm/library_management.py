6083b5f9ffefc266c499be7930877b2ea31d2c82
elammz <elammizanu4@gmail.com>
2024-07-15 23:43:34 +0300

    modified:   programming_paradigm/library_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,6 +1,6 @@
 class Book:
     
-    def init(self, title, author):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
         self._is_checked_out = False
